e1e3945187fd50924c487fe9b1c922ac71bfca1a
Telo PHILIPPE <telo.philippe@gmail.com>
2025-10-04 13:50:28 +0200

    Added hand script

---
+++ scripts/hand_path.gd
@@ -0,0 +1,41 @@
+extends Path2D
+
+@onready var path_2d: Path2D = $"."
+@onready var line_2d: Line2D = $"../Line2D"
+@onready var camera_2d: Camera2D = $"../Camera2D"
+@onready var hand: Sprite2D = $"../Hand"
+
+var cur_length = 0
+var max_length = 50
+
+func update_line():
+	var points = []
+	for i in range(path_2d.curve.point_count):
+		points.append(path_2d.curve.get_point_position(i))
+	line_2d.points = points	
+
+func mouse_input(delta: float) -> void:
+	var count = path_2d.curve.point_count
+	var mouse_pos = get_viewport().get_mouse_position()
+	var offset = camera_2d.position + get_viewport().get_visible_rect().size/2
+	mouse_pos -= offset
+	# path_2d.curve.set_point_position(count-2, mouse_pos - offset)
+	
+	var last_point = path_2d.curve.get_point_position(count - 1)
+	var direction = mouse_pos - last_point
+	last_point += direction * delta
+	
+	path_2d.curve.set_point_position(count - 1, last_point)
+	
+	cur_length = (last_point - path_2d.curve.get_point_position(count - 2)).length()
+	
+	if cur_length > max_length:
+		path_2d.curve.add_point(last_point)
+		
+	hand.position = last_point
+
+func _process(delta: float) -> void:
+	mouse_input(delta)
+	update_line()
+	
+	
