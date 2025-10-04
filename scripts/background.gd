74eca4dc63642829c2e47de9c8ef6a3830d6e09c
Jérémy Gerin <jeremy.gerin84@gmail.com>
2025-10-04 17:37:45 +0200

    signaux

--- scripts/background.gd
+++ scripts/background.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+var speed:float = 100.0
+var isMoving:bool = true
+@onready var guide: Sprite2D = $"../Guide"
+
 var screenSize:Vector2
 @onready var mur: Parallax2D = $mur
 @onready var sol: Parallax2D = $sol
@@ -11,6 +15,20 @@ func resize() -> void:
 	sol.repeat_size.x = screenSize.x
 	colonne.repeat_size.x = screenSize.x
 	
+func move(delta: float) -> void:
+	MainCamera2D.position.x += speed*delta
+	if MainCamera2D.position.x >= screenSize.x:
+		isMoving = false
+		MainCamera2D.position.x -= screenSize.x
+		
+func setIsMoving() -> void:
+	isMoving = true
+	
 func _ready() -> void:
 	get_viewport().size_changed.connect(resize)
 	resize()
+	guide.move.connect(setIsMoving)
+	
+func _process(delta: float) -> void:
+	if isMoving:
+		move(delta)
--- scripts/guide.gd
+++ scripts/guide.gd
@@ -2,9 +2,15 @@ extends Sprite2D
 
 var cam_offset:Vector2
 
+signal move 
+
 func _ready() -> void:
 	cam_offset = MainCamera2D.position - position
 	
 func _process(delta: float) -> void:
-	MainCamera2D.position.x += 100*delta
 	position.x = MainCamera2D.position.x - cam_offset.x
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == KEY_SPACE:
+			move.emit()
