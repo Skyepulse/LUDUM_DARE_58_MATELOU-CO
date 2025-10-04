b0f4f780d3a0342079f90a0dc1555c95b151f20c
Jérémy Gerin <jeremy.gerin84@gmail.com>
2025-10-04 15:31:06 +0200

    ajout MainUI

--- scripts/mainUI.gd
+++ scripts/mainUI.gd
@@ -6,18 +6,16 @@ var minVal: int = 0
 var currentVal: int = 0
 
 func _ready():
-    suspicionBar.min_value = minVal
-    suspicionBar.max_value = maxVal
-    suspicionBar.value = currentVal
+	suspicionBar.min_value = minVal
+	suspicionBar.max_value = maxVal
+	suspicionBar.value = currentVal
 
 func setSuspicion(value: int):
-    currentVal = clamp(value, minVal, maxVal)
-    suspicionBar.value = currentVal
+	currentVal = clamp(value, minVal, maxVal)
+	suspicionBar.value = currentVal
 
 func hideSuspicionBar():
-    suspicionBar.visible = false
+	suspicionBar.visible = false
 
 func showSuspicionBar():
-    suspicionBar.visible = true
-
-
+	suspicionBar.visible = true
