856c584afc11af9dc23a011d7a3c21cb1bff7208
Elmrys <elmrys@gmail.com>
2025-10-06 11:59:42 +0200

    Added level ool

--- scripts/main_game_scene.gd
+++ scripts/main_game_scene.gd
@@ -3,24 +3,21 @@ extends Node2D
 @onready var background: Node2D = $Background
 const OBJECT_BUTTON = preload("uid://bdfxsfkj33qq8")
 
-
+@export var possible_levels: Array[PackedScene] = []
 var rng = RandomNumberGenerator.new()
 
+var last_object_index: int = -1
+
 func instantiate_objects():
-	var count = GameManager.all_indices.size()
-	var index = GameManager.all_indices[rng.randi() % count]
-	
-	GameManager.object_index = index
-	
-	print(count, ", ", index)
-	
 	var screen_size = get_viewport().get_visible_rect().size
-	var offset = Vector2(GameManager.level_index + 1.5, 0.4) * screen_size;
-	var obj = GameManager.infoDictionary[index].scene.instantiate()
-	add_child(obj)
-	
-	obj.scale = Vector2(0.2, 0.2)
-	obj.set_position(offset)
+	var offset = Vector2(GameManager.level_index + 1.5, 0.35) * screen_size;
+	var level = possible_levels[rng.randi() % possible_levels.size()].instantiate()
+	add_child(level)
+
+	level.set_position(offset)
+	last_object_index = level.startup_scene(last_object_index)
+	GameManager.object_index = last_object_index
+	print("Current object index: %d" % GameManager.object_index)
 
 func instantiate_begin_button() -> void:
 	var screen_size = get_viewport().get_visible_rect().size
