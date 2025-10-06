4e728abff04d5ce7e85fb01ca2a80416bd27cd39
Elmrys <elmrys@gmail.com>
2025-10-06 21:11:04 +0200

    updated the levels

--- scripts/obstacle_camera.gd
+++ scripts/obstacle_camera.gd
@@ -12,64 +12,71 @@ var enteredTimes: int = 0
 var flag: bool = false
 
 func _ready():
-    setPoints(rayLength, rayWidth)
-    rotateCamera(initRotation)
+	setPoints(rayLength, rayWidth)
 
 func _process(delta: float) -> void:
-    if flag:
-        GameManager.increaseSuspicion(suspicionIncreaseRate * delta)
+	if flag:
+		GameManager.increaseSuspicion(suspicionIncreaseRate * delta)
 
 func setPoints(length: float, width: float):
-    var shape := ConvexPolygonShape2D.new()
-    shape.points = [
-        Vector2(0, 10),
-        Vector2(length, width / 2),
-        Vector2(length, -width / 2),
-        Vector2(0, -10),
-    ]
+	var shape := ConvexPolygonShape2D.new()
+	shape.points = [
+		Vector2(0, 10),
+		Vector2(length, width / 2),
+		Vector2(length, -width / 2),
+		Vector2(0, -10),
+	]
 
-    var collisionShape := rayBox.get_node("CollisionShape2D") as CollisionShape2D
-    if collisionShape:
-        collisionShape.shape = shape
-        rayVisual.polygon = shape.points
+	var collisionShape := rayBox.get_node("CollisionShape2D") as CollisionShape2D
+	if collisionShape:
+		collisionShape.shape = shape
+		rayVisual.polygon = shape.points
 
 func changeRay(length: float, width: float):
-    rayLength = length
-    rayWidth = width
-    setPoints(rayLength, rayWidth)
+	rayLength = length
+	rayWidth = width
+	setPoints(rayLength, rayWidth)
 
 func rotateCamera(degrees_val: float):
-    rotation_degrees = wrapf(degrees_val, 0.0, 360.0)
+	rotation_degrees = wrapf(degrees_val, 0.0, 360.0)
 
 func _on_ray_box_area_entered(area: Area2D) -> void:
-    if GameManager.isHandRetracting():
-        return
+	if GameManager.isHandRetracting():
+		return
 
-    if area.is_in_group("Player"):
+	if area.is_in_group("Player"):
 
-        if not flag:
-            Signals.emit_signal("activate_vignette_effect")
-            
-        enteredTimes += 1
-        print("Player detected! Entered times: %d" % enteredTimes)
+		if not flag:
+			Signals.emit_signal("activate_vignette_effect")
+			
+		enteredTimes += 1
+		print("Player detected! Entered times: %d" % enteredTimes)
 
-        flag = true
+		flag = true
 
 func _on_ray_box_area_exited(area: Area2D) -> void:
-    if not flag:
-        return
+	if not flag:
+		return
 
-    if area.is_in_group("Player"):
-        if GameManager.isHandRetracting():
-            enteredTimes -= 1
+	if area.is_in_group("Player"):
+		if GameManager.isHandRetracting():
+			enteredTimes -= 1
 
-            print("Player exited! Entered times: %d" % enteredTimes)
-            if enteredTimes <= 0:
-                enteredTimes = 0
-            else:
-                return
+			print("Player exited! Entered times: %d" % enteredTimes)
+			if enteredTimes <= 0:
+				enteredTimes = 0
+			else:
+				return
 
-            flag = false
-            print("Player lost!")
+			flag = false
+			print("Player lost!")
 
-        
+func _on_hit_box_area_exited(area: Area2D) -> void:
+	return
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Player"):
+		if GameManager.isHandRetracting():
+			return
+		
+		GameManager.Player.retract(0.001 * GameManager.Player.get_average_speed())
