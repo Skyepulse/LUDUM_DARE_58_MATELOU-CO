d6b247741565bb54c19c4398790c6132d7359573
Elmrys <elmrys@gmail.com>
2025-10-06 20:04:41 +0200

    Menu fixed

--- scripts/GameUI.gd
+++ scripts/GameUI.gd
@@ -1,12 +1,7 @@
 extends Control
 
-@onready var menu: Control = $Menu
-@onready var play_button: Button = $Menu/PlayButton
-@onready var collection_button: Button = $Menu/CollectionButton
-@onready var credit_button: Button = $Menu/CreditButton
-@onready var quit_button: Button = $Menu/QuitButton
-
-@onready var collection_panel: Panel = $Collection/CollectionPanel
+@onready var collection_panel: Panel = $Collection/Collection/CollectionPanel
+@onready var collection_root: Control = $Collection
 @onready var suspicion_bar: CenterContainer = $SuspicionBar
 @onready var progress_bar: ProgressBar = $SuspicionBar/ProgressBar
 @onready var effects: Control = $Effects
@@ -19,12 +14,9 @@ extends Control
 @export var vignette_max_radius: float = 1.0
 @export var vignette_min_radius: float = 0.6
 
-var screenSize:Vector2
+var state_entered_from_collection = Signals.PAUSED
 
-const PLAY_TEXT: String = "PLAY"
-const COLLECTION_TEXT: String = "COLLECTION"
-const CREDIT_TEXT: String = "CREDIT"
-const QUIT_TEXT: String = "QUIT"
+var screenSize:Vector2
 
 var currentVal: float = 0.0
 
@@ -34,19 +26,6 @@ func setSuspicion(value: float):
 
 func _ready() -> void:
 	screenSize = get_viewport().get_visible_rect().size
-	menu.position = screenSize/2
-	
-	play_button.text = PLAY_TEXT
-	play_button.position.y = -7.5*play_button.size.y
-	
-	collection_button.text = COLLECTION_TEXT
-	collection_button.position.y = -2.5*collection_button.size.y
-	
-	credit_button.text = CREDIT_TEXT
-	credit_button.position.y = 2.5*credit_button.size.y
-	
-	quit_button.text = QUIT_TEXT
-	quit_button.position.y = 7.5*quit_button.size.y
 	
 	collection_panel.hide_collection()
 	suspicion_bar.hideSuspicionBar()
@@ -63,39 +42,8 @@ func _ready() -> void:
 
 	main_menu.visible = true
 	
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_pressed():
-		if event.keycode == KEY_ESCAPE:
-			escape_is_pressed()
-
-func escape_is_pressed() -> void:
-	if Signals.game_state == Signals.PAUSED:
-		play_button_is_pressed()
-	elif Signals.game_state == Signals.INGAME:
-		Signals.game_state = Signals.PAUSED
-		Signals.emit_signal("game_paused")
-		suspicion_bar.hideSuspicionBar()
-		menu.visible = true
-	elif Signals.game_state == Signals.COLLECTION:
-		Signals.game_state = Signals.PAUSED
-		collection_panel.hide_collection()
-		menu.visible = true
-	elif Signals.game_state == Signals.CREDIT:
-		Signals.game_state = Signals.PAUSED
-		menu.visible = true
-		
-func play_button_is_pressed() -> void:
-	if Signals.game_state == Signals.START:
-		Signals.emit_signal("game_started")
-	elif Signals.game_state == Signals.PAUSED:
-		Signals.emit_signal("game_unpaused")
-	Signals.game_state = Signals.INGAME
-	menu.visible = false
-	suspicion_bar.showSuspicionBar()
-	
 func collection_button_is_pressed() -> void:
-	Signals.game_state = Signals.COLLECTION
-	menu.visible = false
+	collection_root.visible = true
 	collection_panel.show_collection()
 	
 func play_vignette_animation():
@@ -126,14 +74,13 @@ func _on_Timer_timeout() -> void:
 func show_game_over_screen() -> void:
 	gameOverScreen.visible = true
 	suspicion_bar.hideSuspicionBar()
-	menu.visible = false
 	collection_panel.hide_collection()
+	Signals.game_state = Signals.PAUSED
 
 func hide_game_over_screen() -> void:
 	gameOverScreen.visible = false
 
 func _on_restart_button_pressed() -> void:
-	suspicion_bar.showSuspicionBar()
 	GameManager.startGame()
 
 func _on_see_collection_button_pressed() -> void:
@@ -141,7 +88,6 @@ func _on_see_collection_button_pressed() -> void:
 
 func _on_start_game_button_pressed() -> void:
 	main_menu.visible = false
-	play_button_is_pressed()
 	GameManager.startGame()
 
 func _on_quit_pressed() -> void:
@@ -149,10 +95,17 @@ func _on_quit_pressed() -> void:
 
 func show_main_menu() -> void:
 	main_menu.visible = true
-	menu.visible = false
+	Signals.game_state = Signals.START
+
 	collection_panel.hide_collection()
+	collection_root.visible = false
+	
 	suspicion_bar.hideSuspicionBar()
 	gameOverScreen.visible = false
 
 func _on_menu_button_pressed() -> void:
 	show_main_menu()
+
+func _on_hide_collection_pressed() -> void:
+	collection_root.visible = false
+	collection_panel.hide_collection()
